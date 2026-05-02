a4f93d10bbc711a4da27af69d4d6035b4287f1f2
julian-nec-ui <julian.neacsu@gmail.com>
2026-05-02 18:19:12 -0500

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** 
+ * @type {import('next').NextConfig}
+ */
+ 
 const nextConfig = {
-  output: "export",
   reactCompiler: true,
   cacheComponents: true
 };
